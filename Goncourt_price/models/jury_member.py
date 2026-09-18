813641f36efd5d79ed4f6b64621cbe8f59ca9cd4
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-09-18 15:11:01 +0200

    updated jury_member.py

--- Goncourt_price/models/jury_member.py
+++ Goncourt_price/models/jury_member.py
@@ -16,14 +16,16 @@ class Jury_member:
     - id_jury_member                 : clé primaire de l'entité persistante
     - is_president                   : is_president est un booleen
     - is_member                      : is_member est un booleen
+    - id_user                        : id de l'utilisateur
     - member_list                    : liste des membres
 
     """
     id_jury_member: Optional[int] = field(default=None, init=False)
     is_president: bool
     is_member: bool
+    id_user: Optional[int] = field(default=None, init=False)
     member_list = []
 
     def __str__(self) -> str:
-        jury_member_str = f" {self.is_president}, {self.is_member}\n"
+        jury_member_str = f" {self.is_president}, {self.is_member}, {self.id_user} \n"
         return jury_member_str
